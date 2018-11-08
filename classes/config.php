6b7180abe20b99228e5c57f4aff197901acc0e87
Biboletin <shady_fan@abv.bg>
2018-11-08 23:24:32 +0200

    Modified files and added new REST project

--- classes/config.php
+++ classes/config.php
@@ -1,6 +1,26 @@
 <?php
+namespace Biboletin;
 
 class Config
 {
-    const HOST = "localhost";
+    private static $config = [
+        "mysql" => [
+            "host" => "127.0.0.1",
+            "user" => "root",
+            "password" => "",
+            "database" => "",
+            "port" => 3306,
+        ],
+    ];
+
+    public static function get($value)
+    {
+        foreach (self::$config as $props => $values) {
+            if (is_array($values)) {
+                foreach ($values as $prop => $value) {
+                    echo $prop . ': ' . $value . '<br/>';
+                }
+            }
+        }
+    }
 }
