73f11dbf8852fc55657fffa64cd337d465d4167a
biboleitn <biboleitn87@gmail.com>
2019-11-11 11:48:43 +0200

    fixed filter class AGAIN

--- classes/filter.php
+++ classes/filter.php
@@ -6,45 +6,51 @@ class Filter
 {
     public function sanitizeInput($data, $flag = ENT_COMPAT, $encoding = "utf-8")
     {
-		if(!is_array($data)){
-			return htmlspecialchars(trim($data), $flag, $encoding);
-		}
-		foreach ($data as $key => $value) {
-			if (is_array($value)) {
-				return sanitize($value);
-			}
-			$data[$key] = htmlspecialchars(trim($value), $flag, $encoding);
-		}
-		return $data;
+        if (!is_array($data)) {
+            return htmlspecialchars(trim($data), $flag, $encoding);
+        }
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                return $this->sanitizeInput($value);
+            }
+            $data[$key] = htmlspecialchars(trim($value), $flag, $encoding);
+        }
+        return $data;
     }
 
-	public function sanitizeOutput($data, $flag = ENT_COMPAT, $encoding = "utf-8")
-	{
-		if (!is_array($data)) {
-			return htmlspecialchars_decode(trim($data));
-		}
-		foreach ($data as $key => $value) {
-			if (is_array($value)) {
-				return sanitizeOutput($value);
-			} else {
-				$data[$key] = htmlspecialchars_decode(trim($value));
-			}
-		}
-		return $data;
-	}
+    public function sanitizeOutput($data, $flag = ENT_COMPAT, $encoding = "utf-8")
+    {
+        if (!is_array($data)) {
+            return htmlspecialchars_decode(trim($data));
+        }
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                return $this->sanitizeOutput($value);
+            }
+			$data[$key] = htmlspecialchars_decode(trim($value));
+        }
+        return $data;
+    }
 
     public function toUTF($data)
     {
-		if (!is_array($data)) {
-			return iconv("windows-1251", "utf-8", $data);
-		}
-		foreach ($data as $key => $value) {
-			if (is_array($value)) {
-				toUTF($value);
-			} else {
-				$data[$key] = iconv("windows-1251", "utf-8", $value);
-			}
-		}
-		return $data;
+        if (!is_array($data)) {
+            return mb_convert_encoding(
+				$data, 
+				"utf-8", 
+				mb_detect_encoding($data, "auto")
+			);
+        }
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                return $this->toUTF($value);
+            }
+			$data[$key] = mb_convert_encoding(
+				$value, 
+				"utf-8", 
+				mb_detect_encoding($value, "auto")
+			);
+        }
+        return $data;
     }
 }
--- classes/index.php
+++ classes/index.php
@@ -6,3 +6,9 @@ include 'session.php';
 include 'token.php';
 include 'filter.php';
 include 'config.php';
+
+use Biboletin\Filter;
+
+$filter = new Filter();
+
+echo $filter->toUTF("да бе да");
